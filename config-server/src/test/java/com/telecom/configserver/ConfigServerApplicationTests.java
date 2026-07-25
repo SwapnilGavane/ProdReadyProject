72d1060decdf8c5ff230d136afce056e1fccd472
Swapnil Gavane <swapnil3212@gmail.com>
2026-07-25 15:32:54 +0530

    Updated code

---
+++ config-server/src/test/java/com/telecom/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telecom.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
